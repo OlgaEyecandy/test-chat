7ab23629334311427d8b889b25eb226214c0e74c
Olga <39336446+OlgaEyecandy@users.noreply.github.com>
2018-07-10 20:52:17 +0400

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
-var timer = new Date();
-
+// jQuery one:
+/* 
 function CommentToMessage(n) {
     var element = document.createElement("div");
     element.innerText = element.textContent = n;
@@ -12,4 +12,17 @@ function web_send_msg() {
     var text = $("#comment-field").val();
     $("#comment-field").val("");
     $("#messages").append("<p><em>" + CommentToMessage(name) + ": </em>" + CommentToMessage(text) + "</p>");
+}
+*/
+//JS one:
+document.getElementById("btn").onclick = getComment;
+
+function getComment() {
+    var text = (document.getElementById("comment-field").value);
+    var name = document.getElementById('comment-name').value;
+    var message = document.createElement('div');
+    message.innerHTML = '<em>' + name + ': ' + '</em>' + text;
+    message.className = 'message';
+    messages.appendChild(message);
+    return text
 }
