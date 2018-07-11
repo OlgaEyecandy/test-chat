04ab1a86edc4d01cdf3d9b14c2742bc4dcd17a06
Olga <39336446+OlgaEyecandy@users.noreply.github.com>
2018-07-11 14:10:49 +0400

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,28 +1,15 @@
-// jQuery one:
-/* 
-function CommentToMessage(n) {
-    var element = document.createElement("div");
-    element.innerText = element.textContent = n;
-    n = element.innerHTML;
-    return n;
-}
-
-function web_send_msg() {
-    var name = $("#comment-name").val();
-    var text = $("#comment-field").val();
-    $("#comment-field").val("");
-    $("#messages").append("<p><em>" + CommentToMessage(name) + ": </em>" + CommentToMessage(text) + "</p>");
-}
-*/
-//JS one:
-document.getElementById("btn").onclick = getComment;
-
 function getComment() {
-    var text = (document.getElementById("comment-field").value);
+    var text = document.getElementById('comment-field').value;
     var name = document.getElementById('comment-name').value;
-    var message = document.createElement('div');
-    message.innerHTML = '<em>' + name + ': ' + '</em>' + text;
-    message.className = 'message';
-    messages.appendChild(message);
-    
+    if (name.length && text.length) {
+        var message = document.createElement('div');
+        message.innerHTML = '<em>' + name + ': ' + '</em>' + text;
+        message.className = 'message';
+        messages.appendChild(message);
+        document.getElementById('comment-field').value = '';
+        document.getElementsByClassName('alert')[0].style.opacity = '0';
+    } else {
+        document.getElementsByClassName('alert')[0].style.opacity = '1';
+    }
+
 }
